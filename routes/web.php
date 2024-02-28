cd3187c53fa5bef8f45d11dd3b0ca6a5f08395f7
intannra <intanrahmadani167@gmail.com>
2024-02-28 21:01:35 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\GaleryController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/',[UserController::class,'login'])->name('login');
+Route::get('/register',[UserController::class,'register']);
+
+Route::post('/simpanreg',[UserController::class,'saveRegister']);
+Route::post('/login',[UserController::class,'proseslogin']);
+
+Route::resource('/galery',GaleryController::class)->middleware('auth'); 
+
+Route::get('/hapusfoto',[GaleryController::class,'show']);
+Route::get('/logout',[UserController::class,'logout']);
